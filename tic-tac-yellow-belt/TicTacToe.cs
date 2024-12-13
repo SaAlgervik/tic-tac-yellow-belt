5b8a5e04eba9f94a7465b71e0077fbce13f823ab
saalgervik <sandra.algervik@winlas.se>
2024-12-13 09:48:11 +0100

    feat: initial game board created and method to print out the bord to the user

--- tic-tac-yellow-belt/TicTacToe.cs
+++ tic-tac-yellow-belt/TicTacToe.cs
@@ -2,6 +2,13 @@
 
 public class TicTacToe
 {
+    private static char[,] _board =
+    {
+        { '1', '2', '3' },
+        { '4', '5', '6' },
+        { '7', '8', '9' }
+    };
+    
     public bool Method1()
     {
         return true;
@@ -9,6 +16,12 @@ public class TicTacToe
 
     public static void PrintBord()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("==========");
+        Console.WriteLine($" {_board[0, 0]} | {_board[0, 1]} | {_board[0, 2]} ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine($" {_board[1, 0]} | {_board[1, 1]} | {_board[1, 2]} ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine($" {_board[2, 0]} | {_board[2, 1]} | {_board[2, 2]} ");
+        Console.WriteLine("==========");
     }
 }
